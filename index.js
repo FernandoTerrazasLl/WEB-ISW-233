0f37bcdea3623ae3dc75e209dd6a012c9ba554cb
fernando <terrazasllanosfernando@gmail.com>
2026-03-12 00:03:31 -0400

    subiendo mutation

--- index.js
+++ index.js
@@ -9,3 +9,50 @@ const db = initMockDB({
 });
 const list = document.getElementById("list");
 const observerElement = document.getElementById("bottom-observer");
+
+//INTERSECTION OBSERVER
+let contadorPagina = 0;
+
+async function cargarDatos() {
+  const datos = await db.getPage(contadorPagina);
+  datos.forEach(item => {
+    const card = createCardElement(item.title, item.body);
+    list.appendChild(card);
+  });
+  contadorPagina++;
+}
+
+const config = {
+  root: null, //QUE VA A MIRAR, EN CASO DE QUE NO HAYA NADA MIRA EL VIEW PORT
+  rootMargin: "0px", //ES COMO UN MARGIN EN WEB, HACE QUE SE ACTIVE EL OBSERVER CUANDO LLEGA A ESTE MARGEN DE ROOT
+  threshold: 0.1
+};
+
+function handleObserver(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      cargarDatos();
+    }
+  });
+}
+const observer = new IntersectionObserver(handleObserver, config);
+observer.observe(observerElement);
+
+//MUTATION OBSERVER
+const mutation_observer=new MutationObserver((mutations)=>{
+  mutations.forEach(mutation => {
+    if (mutation.type === "characterData") {
+        let target=mutation.target;
+
+        const firstThree = target.textContent.slice(0, 3);
+        if (SUPPORTED_ELEMENTS.has(firstThree)) {
+          target.replaceWith(getHeading(target));
+        }
+    }
+  });
+});
+
+mutation_observer.observe(list, {
+  characterData: true,
+  subtree: true
+});
